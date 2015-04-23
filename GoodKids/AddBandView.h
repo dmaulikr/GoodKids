8ef64d38c4254dd32a23c235eb1c7561e2a27a4f
yabdc <dark7898@gmail.com>
2015-04-23 11:34:57 +0800

    v1.0.5
    嘗試自製Ｖiew

---
+++ GoodKids/AddBandView.h
@@ -0,0 +1,13 @@
+//
+//  AddBandView.h
+//  GoodKids
+//
+//  Created by 詹鎮豪 on 2015/4/23.
+//  Copyright (c) 2015年 SuperNova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddBandView : UIView
+
+@end
---
+++ GoodKids/AddBandView.m
@@ -0,0 +1,21 @@
+//
+//  AddBandView.m
+//  GoodKids
+//
+//  Created by 詹鎮豪 on 2015/4/23.
+//  Copyright (c) 2015年 SuperNova. All rights reserved.
+//
+
+#import "AddBandView.h"
+
+@implementation AddBandView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
